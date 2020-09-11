b2c9df8471f0c3f6e826f2927c3fbe3d01454f91
tannershimanek <tannershimanek@gmail.com>
2020-09-11 15:46:07 -0600

    👔  Finished merge sort

--- Project_2/sort.py
+++ Project_2/sort.py
@@ -7,8 +7,12 @@ is_list_sorted = False
 
 
 def quicksort(lyst):
-    # use recursion
-    pass
+    try:
+        list(lyst)
+    except TypeError:
+        print('ERROR: parameter is not a list. Please pass in a list. \n')
+    else:
+        quicksort_helper()
 
 
 def quicksort_helper(low, high, lyst):
@@ -18,14 +22,49 @@ def quicksort_helper(low, high, lyst):
 
 
 def mergesort(lyst):
-    # use recursion
-    pass
+    try:
+        list(lyst)
+    except TypeError:
+        print('ERROR: parameter is not a list. Please pass in a list. \n')
+    else:
+        mergesort_helper(lyst)
+        return lyst
 
 
-def mergesort_helper(low, high, lyst):
+def mergesort_helper(lyst):
     """ The recursive part of mergesort. """
     RecursionCounter() # needed for unit test
-    pass
+
+    if len(lyst) > 1:
+        mid = len(lyst) // 2
+        left = lyst[:mid]
+        right = lyst[mid:]
+
+        mergesort_helper(left)
+        mergesort_helper(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lyst[k] = left[i]
+                i += 1
+            else:
+                lyst[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            lyst[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            lyst[k] = right[j]
+            j += 1
+            k += 1
+
+    return lyst
 
 
 def insertion_sort(lyst):
@@ -61,33 +100,51 @@ def selection_sort(lyst):
 
 
 def timsort(lyst):
-    return sorted(lyst)
+    try:
+        list(lyst)
+    except TypeError:
+        print('ERROR: parameter is not a list. Please pass in a list. \n')
+    else:
+        sorted_lyst = sorted(lyst)
+        return sorted_lyst
 
 
 def is_sorted(lyst):
-    """ Checks if the list passed in is sorted. """
+    """ Checks if the list passed in is sorted. Uses the global variable is_list_sorted. """
     global is_list_sorted
-
-    if all(lyst[i] <= lyst[i + 1] for i in range(len(lyst) - 1)):
-        is_list_sorted = True
+    try:
+        list(lyst)
+    except TypeError:
+        print('ERROR: parameter is not a list. Please pass in a list. \n')
     else:
-        is_list_sorted = False
+        if all(lyst[i] <= lyst[i + 1] for i in range(len(lyst) - 1)):
+            is_list_sorted = True
+        else:
+            is_list_sorted = False
 
 
 def main():
-    DATA_SIZE = 10
+    DATA_SIZE = 10  # change sample size
     random.seed(2)  # saves the generated list to seed(2)
     DATA = random.sample(range(100), k=DATA_SIZE) # [7, 11, 10, 46, 21, 94, 85, 39, 32, 77]
 
     # quick sort
-
+    
 
 
 
     # merge sort
-
-
-
+    print('checking if list is sorted..')
+    is_sorted(DATA.copy())
+    if is_list_sorted == False:
+        print('starting merge sort..')
+        mergesort_start = perf_counter()
+        mergesort(DATA.copy())
+        mergesort_stop = perf_counter()
+        mergesort_time = mergesort_stop - mergesort_start
+        print(f'merge sort duration {"%.10f"%mergesort_time} seconds. \n')
+    elif is_list_sorted == True:
+        print('list sorted')
 
     # insertion sort
     print('checking if list is sorted..')
@@ -128,6 +185,9 @@ def main():
     elif is_list_sorted == True:
         print('List sorted')
 
+
+
+
     # print('UNSORTED RANDARRAY: ', DATA)
 
 
@@ -136,8 +196,8 @@ main()
 
 """ Each function should """
 # verify that the parameter is a list                               []
-# assume the list only contains integers                            []
-# assume the list is randomized                                     []
+# assume the list only contains integers                            [DONE]
+# assume the list is randomized                                     [DONE]
 # each function should return a sorted list                         []
 # must use built in timsort --> dont write yourself                 []
 # use the recursive counter function for quicksort and mergesort    []
@@ -146,21 +206,23 @@ main()
 """ Checklist """
 # quick sort                             []
 # quick sort helper                      []
-# mergesort                              []
-# mergesort helper                       []
+# mergesort                              [DONE]
+# mergesort helper                       [DONE] https://www.educative.io/edpresso/merge-sort-in-python
 # insertion sort                         [DONE] https://www.educative.io/edpresso/how-to-implement-insertion-sort-in-python
 # selection sort                         [DONE] https://www.educative.io/edpresso/how-to-implement-selection-sort-in-python
 # random array                           [DONE]
 # timing for all functions               [IN PROGRESS]
 # main                                   [IN PROGRESS]
-# timing results are 2 are 2 sigfigs     []
+# timing results are are 2 sigfigs       [DONE]
 # use large array size (10,000 - 50,000) []
 # built in timsort                       [DONE] https://realpython.com/sorting-algorithms-python/#implementing-timsort-in-pytho
 # test is_sorted()                       [DONE]
+# is_sorted() should verify list type    [DONE]
+# each function should verify list type  [IN PROGRESS]
 
 
 """ Other """
 # make sure that the lyst is randomized after every function            [FIXED] use DATA.copy()
-# remove the return statement of each function when done                []
+# remove the return statement of each function when done                [REMOVED]
 # meets coding stanadards (8.5+)                                        [IN PROGRESS]
 
