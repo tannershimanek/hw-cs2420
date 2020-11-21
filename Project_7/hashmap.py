f7123c612d551df35a4d58f4d91aa3f795dcdc32
tannershimanek <tannershimanek@gmail.com>
2020-11-21 15:56:01 -0700

    💼 Finished extracting individual words from txt file

--- Project_7/hashmap.py
+++ Project_7/hashmap.py
@@ -1,3 +1,30 @@
+"""
+Author: Tanner Shimanek
+Date: November 21, 2020
+Description: Hashmap ADT
+"""
+
 class HashMap:
     def __init__(self):
+        pass
+
+    def get(self):
+        pass
+
+    def set(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def capacity(self):
+        pass
+
+    def size(self):
+        pass
+
+    def keys(self):
+        pass
+
+    def rehash(self):
         pass
--- Project_7/main.py
+++ Project_7/main.py
@@ -1,4 +1,33 @@
+"""
+Author: Tanner Shimanek
+Date: November 21, 2020
+Description: Driver Module for hashmap.py
+"""
+
+from hashmap import HashMap
+
+
+def clean_line(raw_line):
+    '''Removes all punctuation from input string and returns a
+    list of all worsd which have a lenght greater than one.
+    '''
+    if not isinstance(raw_line, str):
+        raise ValueError("Input must be a string")
+    line = raw_line.strip().lower()
+    line = list(line)
+    for index in range(len(line)):  # pylint: disable-C0200
+        if line[index] < 'a' or line[index] > 'z':
+            line[index] = ' '
+    cleaned = "".join(line)
+    words = [word for word in cleaned.split() if len(word) > 1]
+    return words
+
+
 def main():
-    pass
+    '''Driver function for hashmap.py.'''
+    with open('AliceInWonderland.txt','r') as file:
+        for line in file:
+            print(clean_line(line))
+
 
 main()
